f9099c82dc88dcaf982492f00438b5ed8a6ec952
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2020-09-28 18:02:42 +0200

    first data package version

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Resting heartrate frequency
+#'
+#' A dataset containing the heartrate for 40 individuals, 20 men and 20 women, use to illustrate anova model
+#'
+#' @format A data frame with 40 rows and 3 variables:
+#' \describe{
+#'   \item{freqC}{heartrate}
+#'   \item{Sport}{a sports practice level}
+#'   \item{Sexe}{F for women and H for men}
+#' }
+#' @source \url{https://raw.githubusercontent.com/MarieEtienne/ModStat/master/data/FreqCardiaqueEq.txt}
+"freqdata"
