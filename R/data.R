3fe13f5f116f4de69b0e69a5da5eaf8b7569fc64
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2020-09-29 12:24:45 +0200

    add bats data

--- R/data.R
+++ R/data.R
@@ -9,4 +9,22 @@
 #'   \item{Sexe}{F for women and H for men}
 #' }
 #' @source \url{https://raw.githubusercontent.com/MarieEtienne/ModStat/master/data/FreqCardiaqueEq.txt}
+#' @example data(freqdata)
 "freqdata"
+
+#' Bats analysis of brain size in relation to foraging ecology
+#'
+#' A dataset containing information on Diets as well as morphological information on different bats species
+#'
+#' @format A data frame with 63 rows and 8 variables:
+#' \describe{
+#'   \item{Species}{bats Species}
+#'   \item{Diet}{Diet categories (1 = phytophage ; 2 = gleaner ; 3 = aerial insectivore ; 4 = vampire)}
+#'   \item{BOW}{Body mass}
+#'   \item{BRW}{Brain mass}
+#'   \item{AUD}{Auditory nuclei volume}
+#'   \item{MOB}{Main olfactory bulb volume}
+#'   \item{HIP}{Hippocampus volume}
+#' @source \url{https://biology.ucr.edu/people/faculty/Garland/HutcEA02.pdf}
+#' @example data(bats)
+"bats"
